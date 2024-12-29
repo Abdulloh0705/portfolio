1a15320e819647dd0b1f88a7b18ca099a860746f
haydaraliyevjasurbek <jasurbekhaydaraliyev7777@gmail.com>
2024-12-29 19:14:43 +0500

    kkkk

--- src/components/cards/CardsPage/Pages.jsx
+++ src/components/cards/CardsPage/Pages.jsx
@@ -1,28 +1,33 @@
-import React from 'react'
-import "./pages.scss"
-import "./page.js"
-import ReactPaginate from 'react-paginate'
+import React from 'react';
+import "./pages.scss";
+import { useDispatch } from 'react-redux';
+import { setOffset } from './page'; 
+import ReactPaginate from 'react-paginate';
+
 const Pages = () => {
-  const handleClick = (pag) => {
-    console.log(pag);
-  }
+  const dispatch = useDispatch();
+
+  const handlePageChange = ({ selected }) => {
+    dispatch(setOffset(selected)); 
+  };
+
   return (
     <>
       <ReactPaginate
-        className='paginate'
-        pageCount={8}
+        className="paginate"
+        pageCount={8} 
         previousLabel={'<<'}
         nextLabel={'>>'}
-        pageLinkClassName='paginate_link'
-        activeLinkClassName='paginate_active'
-        previousClassName='paginate_prev'
-        nextClassName='paginate_next'
-        onPageChange={handleClick}
+        pageLinkClassName="paginate_link"
+        activeLinkClassName="paginate_active"
+        previousClassName="paginate_prev"
+        nextClassName="paginate_next"
+        onPageChange={handlePageChange} 
         marginPagesDisplayed={1}
+        pageRangeDisplayed={3}
       />
     </>
+  );
+};
 
-  )
-}
-
-export default Pages
+export default Pages;
--- src/components/cards/CardsPage/page.js
+++ src/components/cards/CardsPage/page.js
@@ -1,17 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = {
+  order: '', 
+  limit: 8, // Har bir sahifada 8 ta mahsulot
+  offset: 0, // Sahifa boshidan mahsulotlar indeksini hisoblash uchun
+};
 
-const initialState ={
-    order: '', 
-    limit: 6,
-    offdet: 0,
-}
-export const page = createSlide ({
-    name: "product",
-    initialState,
-    reducers: {
-        setOrder: (state, action) => {
-            state.order = action.payload
-        },
-    }
-})
-export const {setOrder} =productStore.actions
+export const page = createSlice({
+  name: "product",
+  initialState,
+  reducers: {
+    setOrder: (state, action) => {
+      state.order = action.payload;
+    },
+    setOffset: (state, action) => {
+      state.offset = action.payload * state.limit; // Sahifadan boshlab mahsulotni hisoblash
+    },
+  },
+});
+
+export const { setOrder, setOffset } = page.actions;
+export default page.reducer;
